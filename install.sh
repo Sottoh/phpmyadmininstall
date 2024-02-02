e775c0c4c4e9fa1391571f128deae6a9123f123d
Sottoh <81654603+Sottoh@users.noreply.github.com>
2024-02-02 10:56:45 +0100

    Fixed typos

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-function isroot () {
-    if [[ $EUID -ne 0]]: then
-        echo "Please try running the script as root"
+function isroot() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "Please try runnig the script as sudo"
         exit 1
     fi
 
@@ -10,9 +10,9 @@ function isroot () {
 }
 
 function repositories () {
-    ehco ""
+    echo ""
 
-    ehco "Updating repositories"
+    echo "Updating repositories"
 
     if sudo apt-get update &> /dev/null; then
         echo -e "   ➥\e[32mSuccessfully\e[0m updated the repositories!"
@@ -21,20 +21,28 @@ function repositories () {
     install
 }
 
-echo ""
+function install (){
+    echo ""
 
-echo "Install needed programs"
+    echo "Installing needed programs"
 
-if sudo apt-get install nginx -y &> /dev/null; then
-    echo -e "   ➥\e[32msuccessfully\e[0m installed NGINX!"
-else
-    echo -e "   ➥\e[31mFailed\e[0m installed NGINX"
-fi
+    if sudo apt-get install nginx -y &> /dev/null; then
+        echo -e "   ➥\e[32msuccessfully\e[0m installed NGINX!"
+    else
+        echo -e "   ➥\e[31mFailed\e[0m to install NGINX"
+    fi
 
-if sudo apt-get install phpmyadmin -y &> /dev/null; then
-    echo -e "   ➥\e[32msuccessfully\e[0m installed PhpMyAdmin!"
-else
-    echo -e "   ➥\e[31mFailed\e[0m installed PhpMyAdmin"
-fi
+    if sudo apt-get install mysql-server -y &> /dev/null; then
+        echo -e "   ➥\e[32msuccessfully\e[0m installed MySQL!"
+    else
+        echo -e "   ➥\e[31mFailed\e[0m to install MySQL"
+    fi
+
+    if sudo apt-get install phpmyadmin -y > /dev/null 2>&1; then
+        echo -e "   ➥\e[32msuccessfully\e[0m installed PhpMyAdmin!"
+    else
+        echo -e "   ➥\e[31mFailed\e[0m to install PhpMyAdmin"
+    fi
+}
 
 isroot
