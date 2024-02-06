ed3ffda83bb60fcd6a304e32a639f83c8ab1b05b
Sottoh <81654603+Sottoh@users.noreply.github.com>
2024-02-06 12:16:11 +0100

    Anded detection for Debian-based distributions

--- install.sh
+++ install.sh
@@ -6,6 +6,29 @@ function isroot() {
         exit 1
     fi
 
+    checkos
+}
+
+function checkos() {
+    echo ""
+
+    # Check for /etc/debian_version file
+    if [ -f "/etc/debian_version" ]; then
+        echo "This script can be used on your distribution."
+    exit 0
+    fi
+
+    # Backup check: Look for some core Debian utilities
+    if command -v apt-get &>/dev/null && command -v dpkg &>/dev/null; then
+        echo "This script can be used on your distribution."
+    exit 0
+    fi
+
+    echo "This script is intended for Debian-based distributions only."
+    exit 1
+
+    echo ""
+
     repositories
 }
 
@@ -14,7 +37,7 @@ function repositories () {
 
     echo "Updating repositories"
 
-    if sudo apt-get update &> /dev/null; then
+    if sudo apt update &> /dev/null; then
         echo -e "   ➥\e[32mSuccessfully\e[0m updated the repositories!"
     fi
 
@@ -26,19 +49,19 @@ function install (){
 
     echo "Installing needed programs"
 
-    if sudo apt-get install nginx -y &> /dev/null; then
+    if sudo apt install nginx -y &> /dev/null; then
         echo -e "   ➥\e[32msuccessfully\e[0m installed NGINX!"
     else
         echo -e "   ➥\e[31mFailed\e[0m to install NGINX"
     fi
 
-    if sudo apt-get install mysql-server -y &> /dev/null; then
+    if sudo apt install mysql-server -y &> /dev/null; then
         echo -e "   ➥\e[32msuccessfully\e[0m installed MySQL!"
     else
         echo -e "   ➥\e[31mFailed\e[0m to install MySQL"
     fi
 
-    if sudo apt-get install phpmyadmin -y > /dev/null 2>&1; then
+    if sudo apt install phpmyadmin -y > /dev/null 2>&1; then
         echo -e "   ➥\e[32msuccessfully\e[0m installed PhpMyAdmin!"
     else
         echo -e "   ➥\e[31mFailed\e[0m to install PhpMyAdmin"
